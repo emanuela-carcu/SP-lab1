f9091acca60d23d63da232bccb5d0aeba55364ff
carcu <emanuela.carcu01@e-uvt.ro>
2022-10-24 21:16:35 +0300

    lab1

--- src/Main.java
+++ src/Main.java
@@ -13,24 +13,4 @@ public class Main {
         discoTitanic.print();
     }
 
-//    public static void main(String[] args) {
-//        int a = Integer.parseInt(args[0]);
-//        int b = Integer.parseInt(args[1]);
-//
-//        System.out.println(cmmdc(a,b));
-//    }
-//
-//    public static int cmmdc(int a, int b) {
-//        if(a*b==0)
-//            return a+b;
-//
-//        while (a != b) {
-//            if (a > b)
-//                a = a - b;
-//            else
-//                b = b - a;
-//        }
-//
-//        return a;
-//    }
 }
